d1e43a1d8d353ea3243367d9a4628372a8292487
Roberto Dab <roberto.dabb@gmail.com>
2023-02-10 19:09:24 +0100

    Aggiungere i file di progetto.

---
+++ Silk BLUD Gest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Silk_BLUD_Gest.MvcApplication" Language="C#" %>
---
+++ Silk BLUD Gest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("Silk_BLUD_Gest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Silk_BLUD_Gest")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta il valore di ComVisible su falso, i tipi nell'assembly non sono più visibili
+// dai componenti COM. Se è necessario accedere al tipo nell'assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato per creare l'ID della typelib
+[assembly: Guid("cf5ee1c5-2fb7-4787-af2d-2b36b79f63b1")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai quattro valori seguenti:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero build
+//      Revisione
+//
+// È possibile specificare tutti i valori o lasciare i valori predefiniti per Revisione e Numeri build
+// utilizzando l'asterisco (*) come illustrato di seguito:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
