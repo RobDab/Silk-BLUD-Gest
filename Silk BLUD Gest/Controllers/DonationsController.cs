17e351dce58a115af2f19e4b43e6c8cb6a1044d2
Roberto Dab <roberto.dabb@gmail.com>
2023-02-11 14:31:45 +0100

    Added Entity Controllers | APPRole model as role provider

---
+++ Silk BLUD Gest/Controllers/DonationsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Silk_BLUD_Gest.Models;
+
+namespace Silk_BLUD_Gest.Controllers
+{
+    public class DonationsController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Donations
+        public ActionResult Index()
+        {
+            var donations = db.Donations.Include(d => d.Donors);
+            return View(donations.ToList());
+        }
+
+        // GET: Donations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donations donations = db.Donations.Find(id);
+            if (donations == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donations);
+        }
+
+        // GET: Donations/Create
+        public ActionResult Create()
+        {
+            ViewBag.DonorID = new SelectList(db.Donors, "DonorID", "Name");
+            return View();
+        }
+
+        // POST: Donations/Create
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DonationID,DonorID,FreezingDate,Quantity,BottleNum")] Donations donations)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donations.Add(donations);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DonorID = new SelectList(db.Donors, "DonorID", "Name", donations.DonorID);
+            return View(donations);
+        }
+
+        // GET: Donations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donations donations = db.Donations.Find(id);
+            if (donations == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DonorID = new SelectList(db.Donors, "DonorID", "Name", donations.DonorID);
+            return View(donations);
+        }
+
+        // POST: Donations/Edit/5
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DonationID,DonorID,FreezingDate,Quantity,BottleNum")] Donations donations)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donations).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DonorID = new SelectList(db.Donors, "DonorID", "Name", donations.DonorID);
+            return View(donations);
+        }
+
+        // GET: Donations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donations donations = db.Donations.Find(id);
+            if (donations == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donations);
+        }
+
+        // POST: Donations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Donations donations = db.Donations.Find(id);
+            db.Donations.Remove(donations);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Silk BLUD Gest/Controllers/DonorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Silk_BLUD_Gest.Models;
+
+namespace Silk_BLUD_Gest.Controllers
+{
+    public class DonorsController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Donors
+        public ActionResult Index()
+        {
+            return View(db.Donors.ToList());
+        }
+
+        // GET: Donors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donors donors = db.Donors.Find(id);
+            if (donors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donors);
+        }
+
+        // GET: Donors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Donors/Create
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DonorID,Name,Lastname,Address,Contact,Active,DeliveryDate,GestationWeek,DonorSince,DonorTo,ClinicalNotes,BreastPumpProvided,BreastPumpTaken,TotalDonated")] Donors donors)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donors.Add(donors);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(donors);
+        }
+
+        // GET: Donors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donors donors = db.Donors.Find(id);
+            if (donors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donors);
+        }
+
+        // POST: Donors/Edit/5
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DonorID,Name,Lastname,Address,Contact,Active,DeliveryDate,GestationWeek,DonorSince,DonorTo,ClinicalNotes,BreastPumpProvided,BreastPumpTaken,TotalDonated")] Donors donors)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donors).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(donors);
+        }
+
+        // GET: Donors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donors donors = db.Donors.Find(id);
+            if (donors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donors);
+        }
+
+        // POST: Donors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Donors donors = db.Donors.Find(id);
+            db.Donors.Remove(donors);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
